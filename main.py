db64b60d01bd13c7575a4d833379fa18cd62917a
Ramisetty Sri vasu <srivasuramisetty@gmail.com>
2026-06-03 14:13:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ print("KEY LOADED:", api_key)
 
 client = OpenAI(
     api_key=api_key,
-    base_url="https://gen-ai-be-3.onrender.com"
+    base_url="https://api.groq.com/openai/v1"
 )
 
 print("KEY:", os.getenv("GROQ_API_KEY"))
